09eb0cc8cfa44a225d44a44d390f22347217f1e9
WG37 <willg2070@gmail.com>
2025-07-11 13:34:39 +1000

    fix(namespace): corrected namespaces

--- FE/FE/src/interfaces/game.ts
+++
@@ -1,4 +0,0 @@
-export interface RuleDto { divisor: number; word: string; }
-export interface GameDto { id: number; name: string; author: string; rules: RuleDto[]; /* ... */ }
-export interface CreateGameDto { name: string; author: string; minRange: number; maxRange: number; rules: RuleDto[]; }
-export interface UpdateGameDto extends CreateGameDto {}
